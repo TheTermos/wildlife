2b5e4470cde4662391eb530ce2341bc0f4c76e24
FaceDeer <derksenmobile@gmail.com>
2020-02-21 10:01:06 -0700

    use mobkit.get_nodes_in_area, deal only the worst damage once

--- init.lua
+++ init.lua
@@ -27,11 +27,16 @@ local function node_dps_dmg(self)
 	local box = self.object:get_properties().collisionbox
 	local pos1 = {x = pos.x + box[1], y = pos.y + box[2], z = pos.z + box[3]}
 	local pos2 = {x = pos.x + box[4], y = pos.y + box[5], z = pos.z + box[6]}
-	local node_pos, node_count = minetest.find_nodes_in_area(pos1, pos2, dps_nodes_names)
+	local nodes_overlap = mobkit.get_nodes_in_area(pos1, pos2)
 	local total_damage = 0
-	for name, count in pairs(node_count) do
-		total_damage = total_damage + dps_nodes_damage[name] * count
+
+	for node_def, _ in pairs(nodes_overlap) do
+		local dps = node_def.damage_per_second
+		if dps then
+			total_damage = math.max(total_damage, dps)
+		end
 	end
+
 	if total_damage ~= 0 then
 		mobkit.hurt(self, total_damage)
 	end
